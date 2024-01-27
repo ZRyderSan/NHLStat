24446df3b51c9b1a8b78eb9f130906d30dca4b5b
ZRyderSan <104546541+ZRyderSan@users.noreply.github.com>
2024-01-27 17:43:40 +0800

    Update Class1.cs

--- WpfApp1/Class1.cs
+++ WpfApp1/Class1.cs
@@ -12,10 +12,10 @@ namespace WpfApp1
     {
         
         //string selec = "SELECT * FROM List";
-        public void sqlC(string Team)
+        public void sqlC(string Team,BitmapImage ima)
         {
             string cstring = "Data Source=\\db.db";
-            string inser = $"INSERT INTO Teams Values(NULL,{0},{1},NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)";{Team};;
+            string inser = $"INSERT INTO Teams Values(NULL,{0},{1},NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)";{Team,ima};
             using (var constr = new SQLiteConnection())
             {
                 SQLiteCommand command = new SQLiteCommand(inser, constr);
